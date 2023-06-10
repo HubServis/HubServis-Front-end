472fe1649090c987175210b143191f71d02d3609
Junior042 <eduardoalvesdeoliveirajunior@gmail.com>
2023-06-10 18:29:01 -0300

    changes implemented

--- src/pages/ViewService/index.jsx
+++ src/pages/ViewService/index.jsx
@@ -15,11 +15,13 @@ const ViewService = () => {
 				<a href="http://">Anuncios e eventos</a>
 				<a href="http://">Suporte</a>
 			</AppBar>
-			<main className="flex justify-center mt-[60px] px-28">
+			<main className="flex justify-center mt-[60px] px-28 mb-40">
 				<section className="w-full max-w-[50%] pr-[34px]">
 					<Slider />
 					<article>
-						<h2 className="h2-title-section mt-[90px]">Descrição</h2>
+						<h2 className="h2-title-section mt-[90px]">
+							Descrição
+						</h2>
 						<p className="w-full flex flex-col">
 							<span>
 								Lorem ipsum dolor sit amet consectetur
@@ -44,20 +46,23 @@ const ViewService = () => {
 					<h1 className="font-[700] text-3xl mb-10">
 						Diva Unhas Decoradas Manicure
 					</h1>
-					<Avaliations />
+					<Avaliations size={32}/>
 					<p className="text-xl font-bold text-[color:var(--strong-green)] my-10">
 						56 avaliações sobre esse serviço
 					</p>
 					<hr />
 					<p className="font-bold text-[36px] my-7">R$ 39,99</p>
 					<p className="text-2xl mb-8">Por par de mão</p>
-					<p className="text-base font-bold mb-[50px]">Opção de pix e debito</p>
+					<p className="text-base font-bold mb-[50px]">
+						Opção de pix e debito
+					</p>
 					<BtnFillGreen
+						width="full"
 						onclick={() => console.log("view all services button")}
 					>
-						Explorar
+						Agendar serviço/atendimento
 					</BtnFillGreen>
-					<hr className="mt-[26px] mb-8"/>
+					<hr className="mt-[26px] mb-8" />
 					<p className="text-xl font-normal mb-10">
 						Outros serviços que talves você se interesse
 					</p>
