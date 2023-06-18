1dc33ed62ce3e48f7cb03207526611efd662b282
Junior042 <eduardoalvesdeoliveirajunior@gmail.com>
2023-06-18 16:33:30 -0300

    menu sidebar

---
+++ src/pages/TestePage/TestePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TestePage = () => {
+    return (
+		<>
+			<nav className="max-w-[296px] h-screen bg-[var(--black)]">
+				
+			</nav>
+		</>
+	);
+}
+ 
+export default TestePage;
