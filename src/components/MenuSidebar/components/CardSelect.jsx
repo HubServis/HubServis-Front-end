5ebdd4f5daa6c1690e8b2c4bba93b24a47f2eb4d
Junior042 <eduardoalvesdeoliveirajunior@gmail.com>
2023-06-26 09:22:30 -0300

    sidebar menu open finished

--- src/components/MenuSidebar/components/CardSelect.jsx
+++ src/components/MenuSidebar/components/CardSelect.jsx
@@ -8,7 +8,7 @@ const CardSelect = ({children, open = false, clickFunction = () => {}, title = "
             <div className='items-center hover:bg-[var(--medium-light-black)] rounded-xl mx-4 py-[10px] px-[15px] flex justify-between my-1' onClick={() => clickFunction()}>
                 <span className='flex items-center gap-4'>
                     {icon}
-                    <p className='text-[var(--gray)] text-lg'>{title}</p>
+                    <p className='text-[var(--gray)] text-xs'>{title}</p>
                 </span>
                 {
                     open ? <CaretUp fill={gray} size={20} className="" /> :
