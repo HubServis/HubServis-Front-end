f212991183eaf457a13778072933288c3c0b45e6
Junior042 <eduardoalvesdeoliveirajunior@gmail.com>
2023-06-27 15:00:26 -0300

    fix and changes

--- src/pages/Scheduling-management/components/calendar/index.jsx
+++ src/pages/Scheduling-management/components/calendar/index.jsx
@@ -1,84 +1,225 @@
-import React, { useState } from "react";
-import "./style.css";
-import { ArrowCircleLeft, ArrowCircleRight } from "@phosphor-icons/react";
-import { DAYS, MONTHS } from "./count";
+import { useRef, useState } from "react";
 import {
-	range,
+	ArrowCircleLeft,
+	ArrowCircleRight,
+	Trash,
+	X,
+} from "@phosphor-icons/react";
+import {
+	SevenColGrid,
+	Wrapper,
+	HeadDays,
+	DateControls,
+	StyledEvent,
+	SeeMore,
+	PortalWrapper,
+} from "./styled";
+import { DAYS, MOCKAPPS, MONTHS } from "./conts";
+import {
+	datesAreOnSameDay,
+	getDarkColor,
 	getDaysInMonth,
 	getSortedDays,
-	getDateObj,
-	areDatesTheSame,
-	getRandomDarkColor,
+	nextMonth,
+	prevMonth,
 } from "./utils";
-import { CalenderBody, StyledDay, StyledEvents } from "./styled";
 
-const CalendarScheduling = ({ startingDate, eventsArr, addEvent }) => {
-	const [currentMonth, setCurrentMonth] = useState(startingDate.getMonth());
-	const [currentYear, setCurrentYear] = useState(startingDate.getFullYear());
-	const DAYSINAMONTH = getDaysInMonth(currentMonth, currentYear);
+const CalendarScheduling = () => {
+	const [currentMonth, setCurrentMonth] = useState(new Date().getMonth());
+	const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
+	const [currentDate, setCurrentDate] = useState(new Date());
+	const [events, setEvents] = useState(MOCKAPPS);
+	const dragDateRef = useRef();
+	const dragindexRef = useRef();
+	const [showPortal, setShowPortal] = useState(false);
+	const [portalData, setPortalData] = useState({});
 
-	const nextMonth = () => {
-		if (currentMonth < 11) {
-			setCurrentMonth((prev) => prev + 1);
-		} else {
-			setCurrentMonth(0);
-			setCurrentYear((prev) => prev + 1);
+	const addEvent = (date, event) => {
+		if (!event.target.classList.contains("StyledEvent")) {
+			const text = window.prompt("name");
+			if (text) {
+				date.setHours(0);
+				date.setSeconds(0);
+				date.setMilliseconds(0);
+				setEvents((prev) => [
+					...prev,
+					{ date, title: text, color: getDarkColor() },
+				]);
+			}
 		}
 	};
 
-	const prevMonth = () => {
-		if (currentMonth > 0) {
-			setCurrentMonth((prev) => prev - 1);
-		} else {
-			setCurrentMonth(11);
-			setCurrentYear((prev) => prev - 1);
-		}
+	const drag = (index, e) => {
+		dragindexRef.current = { index, target: e.target };
 	};
 
-	const onAddEvent = (date) => {
-		addEvent(date, getRandomDarkColor());
-	}
+	const onDragEnter = (date, e) => {
+		e.preventDefault();
+		dragDateRef.current = { date, target: e.target.id };
+	};
+
+	const drop = (ev) => {
+		ev.preventDefault();
+
+		setEvents((prev) =>
+			prev.map((ev, index) => {
+				if (index === dragindexRef.current.index) {
+					ev.date = dragDateRef.current.date;
+				}
+				return ev;
+			})
+		);
+	};
+
+	const handleOnClickEvent = (event) => {
+		setShowPortal(true);
+		setPortalData(event);
+	};
+
+	const handlePotalClose = () => setShowPortal(false);
+
+	const handleDelete = () => {
+		setEvents((prevEvents) =>
+			prevEvents.filter((ev) => ev.title !== portalData.title)
+		);
+		handlePotalClose();
+	};
 
 	return (
-		<>
-			<div className="w-full h-screen  border border-solid m-[5px]">
-				<div className="w-full flex h-10 justify-around items-center text-2xl">
-					<ArrowCircleLeft size={32} onClick={prevMonth} />
-					<p>
-						{MONTHS[currentMonth]} {currentYear}
-					</p>
-					<ArrowCircleRight size={32} onClick={nextMonth} />
-				</div>
-				<div className="sevenColorGrid">
-					{getSortedDays(currentMonth, currentYear).map((day, index) => (
-						<span className="text-center bg-green-400 text-xl" key={index}>{day}</span>
-					))}
-				</div>
-				<CalenderBody fourCol={DAYSINAMONTH === 28}>
-					{range(DAYSINAMONTH).map((day, index) => (
-						<StyledDay key={index}
-							onClick={() =>
-								onAddEvent(getDateObj(day, currentMonth, currentYear))
-							}
-							active={areDatesTheSame(
-								new Date(),
-								getDateObj(day, currentMonth, currentYear)
-							)}
+		<Wrapper>
+			<DateControls>
+				<ArrowCircleLeft
+					size={32}
+					onClick={() => prevMonth(currentDate, setCurrentDate)}
+				/>
+				{MONTHS[currentMonth]} {currentYear}
+				<ArrowCircleRight
+					size={32}
+					onClick={() => nextMonth(currentDate, setCurrentDate)}
+				/>
+			</DateControls>
+			<SevenColGrid>
+				{DAYS.map((day, index) => (
+					<HeadDays className="nonDRAG" key={index}>
+						{day}
+					</HeadDays>
+				))}
+			</SevenColGrid>
+
+			<SevenColGrid
+				fullheight={true}
+				is28Days={getDaysInMonth(currentDate) === 28}
+			>
+				{getSortedDays(currentDate).map((day, index) => (
+					<div
+						key={index}
+						id={`${currentDate.getFullYear()}/${currentDate.getMonth()}/${day}`}
+						onDragEnter={(e) =>
+							onDragEnter(
+								new Date(
+									currentDate.getFullYear(),
+									currentDate.getMonth(),
+									day
+								),
+								e
+							)
+						}
+						onDragOver={(e) => e.preventDefault()}
+						onDragEnd={drop}
+						onClick={(e) =>
+							addEvent(
+								new Date(
+									currentDate.getFullYear(),
+									currentDate.getMonth(),
+									day
+								),
+								e
+							)
+						}
+					>
+						<span
+							className={`nonDRAG ${
+								datesAreOnSameDay(
+									new Date(),
+									new Date(
+										currentDate.getFullYear(),
+										currentDate.getMonth(),
+										day
+									)
+								)
+									? "active"
+									: ""
+							}`}
 						>
-							<p>{day}</p>
-							{eventsArr.map(
-								(event, index) =>
-									areDatesTheSame(
-										getDateObj(day, currentMonth, currentYear),
-										event.date
-									) && <StyledEvents key={index} bgColor={event?.color}>{event.title}</StyledEvents>
+							{day}
+						</span>
+						<EventWrapper>
+							{events.map(
+								(ev, index) =>
+									datesAreOnSameDay(
+										ev.date,
+										new Date(
+											currentDate.getFullYear(),
+											currentDate.getMonth(),
+											day
+										)
+									) && (
+										<StyledEvent
+											onDragStart={(e) => drag(index, e)}
+											onClick={() => handleOnClickEvent(ev)}
+											draggable
+											className="StyledEvent"
+											id={`${ev.color} ${ev.title}`}
+											key={ev.title}
+											bgColor={ev.color}
+										>
+											{ev.title}
+										</StyledEvent>
+									)
 							)}
-						</StyledDay>
-					))}
-				</CalenderBody>
-			</div>
-		</>
+						</EventWrapper>
+					</div>
+				))}
+			</SevenColGrid>
+			{showPortal && (
+				<Portal
+					{...portalData}
+					handleDelete={handleDelete}
+					handlePotalClose={handlePotalClose}
+				/>
+			)}
+		</Wrapper>
 	);
 };
 
 export default CalendarScheduling;
+
+const EventWrapper = ({ children }) => {
+	if (children.filter((child) => child).length)
+		return (
+			<>
+				{children}
+				{children.filter((child) => child).length > 2 && (
+					<SeeMore
+						onClick={(e) => {
+							e.stopPropagation();
+							console.log("clicked p");
+						}}
+					>
+						Ver mais...
+					</SeeMore>
+				)}
+			</>
+		);
+};
+
+const Portal = ({ title, date, handleDelete, handlePotalClose }) => {
+	return (
+		<PortalWrapper>
+			<h2>{title}</h2>
+			<p>{date.toDateString()}</p>
+			<Trash onClick={handleDelete} size={32} />
+			<X onClick={handlePotalClose} size={32} />
+		</PortalWrapper>
+	);
+};
--- src/pages/TestePage/TestePage.jsx
+++ src/pages/TestePage/TestePage.jsx
@@ -1,25 +1,12 @@
-import React, {useState} from 'react';
-import {MenuSidebar} from '../../components';
+import React from 'react';
 import CalendarScheduling from '../Scheduling-management/components/calendar';
-import { MOCKEVENTS } from '../Scheduling-management/components/calendar/count';
 
 const TestePage = () => {
-	const [events, setEvents] = useState(MOCKEVENTS);
-
-	
-	const addEvent = (date, color) => {
-		const text = window.prompt('text for event');
-		setEvents(prev => [...prev, {date, title: text, color}]);
-	}
 
     return (
 			<>
 				{/* <MenuSidebar /> */}
-				<CalendarScheduling
-					startingDate={new Date()}
-					eventsArr={events}
-					addEvent={addEvent}
-				/>
+				<CalendarScheduling/>
 			</>
 		);
 }
