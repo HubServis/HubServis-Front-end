42142cd9e113283f4812c4a393bd1a693c7a81d6
Junior042 <eduardoalvesdeoliveirajunior@gmail.com>
2023-06-27 13:00:24 -0300

    prev and next month created

--- src/pages/Scheduling-management/components/calendar/index.jsx
+++ src/pages/Scheduling-management/components/calendar/index.jsx
@@ -2,7 +2,13 @@ import React, { useState } from "react";
 import "./style.css";
 import { ArrowCircleLeft, ArrowCircleRight } from "@phosphor-icons/react";
 import { DAYS, MONTHS } from "./count";
-import { range, getDaysInMonth, getSortedDays, getDateObj, areDatesTheSame } from "./utils";
+import {
+	range,
+	getDaysInMonth,
+	getSortedDays,
+	getDateObj,
+	areDatesTheSame,
+} from "./utils";
 import { CalenderBody, StyledDay } from "./styled";
 
 const CalendarScheduling = ({ startingDate }) => {
@@ -10,17 +16,33 @@ const CalendarScheduling = ({ startingDate }) => {
 	const [currentYear, setCurrentYear] = useState(startingDate.getFullYear());
 	const DAYSINAMONTH = getDaysInMonth(currentMonth, currentYear);
 
-	// parou aqui: https://youtu.be/gcRZ8Ysgquc?t=934
+	const nextMonth = () => {
+		if (currentMonth < 11) {
+			setCurrentMonth((prev) => prev + 1);
+		} else {
+			setCurrentMonth(0);
+			setCurrentYear((prev) => prev + 1);
+		}
+	};
+
+	const prevMonth = () => {
+		if (currentMonth > 0) {
+			setCurrentMonth((prev) => prev - 1);
+		} else {
+			setCurrentMonth(11);
+			setCurrentYear((prev) => prev - 1);
+		}
+	};
 
 	return (
 		<>
 			<div className="w-full h-screen  border border-solid m-[5px]">
 				<div className="w-full flex h-10 justify-around items-center text-2xl">
-					<ArrowCircleLeft size={32} />
+					<ArrowCircleLeft size={32} onClick={prevMonth} />
 					<p>
 						{MONTHS[currentMonth]} {currentYear}
 					</p>
-					<ArrowCircleRight size={32} />
+					<ArrowCircleRight size={32} onClick={nextMonth} />
 				</div>
 				<div className="sevenColorGrid">
 					{getSortedDays(currentMonth, currentYear).map((day) => (
