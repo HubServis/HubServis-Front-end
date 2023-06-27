bbd5fd7bb6313a970e27861f77ad1de1834b637f
Junior042 <eduardoalvesdeoliveirajunior@gmail.com>
2023-06-27 03:11:19 -0300

    dynamic date and styled components integred

--- src/pages/Scheduling-management/components/calendar/index.jsx
+++ src/pages/Scheduling-management/components/calendar/index.jsx
@@ -2,13 +2,15 @@ import React, { useState } from "react";
 import "./style.css";
 import { ArrowCircleLeft, ArrowCircleRight } from "@phosphor-icons/react";
 import { DAYS, MONTHS } from "./count";
-import { range, getDaysInMonth } from "./utils";
+import { range, getDaysInMonth, getSortedDays, getDateObj, areDatesTheSame } from "./utils";
+import { CalenderBody, StyledDay } from "./styled";
 
 const CalendarScheduling = ({ startingDate }) => {
 	const [currentMonth, setCurrentMonth] = useState(startingDate.getMonth());
 	const [currentYear, setCurrentYear] = useState(startingDate.getFullYear());
+	const DAYSINAMONTH = getDaysInMonth(currentMonth, currentYear);
 
-	// parou aqui, integrar styledComponents no projeto: https://youtu.be/gcRZ8Ysgquc?t=662
+	// parou aqui: https://youtu.be/gcRZ8Ysgquc?t=934
 
 	return (
 		<>
@@ -20,20 +22,23 @@ const CalendarScheduling = ({ startingDate }) => {
 					</p>
 					<ArrowCircleRight size={32} />
 				</div>
-
 				<div className="sevenColorGrid">
-					{DAYS.map((day) => (
+					{getSortedDays(currentMonth, currentYear).map((day) => (
 						<span className="text-center bg-green-400 text-xl">{day}</span>
 					))}
 				</div>
-
-				<div className="CalenderBody">
-					{range(getDaysInMonth(currentMonth, currentYear)).map((day) => (
-						<span className="border-black border-[1px] text-right p-2 ">
+				<CalenderBody fourCol={DAYSINAMONTH === 28}>
+					{range(DAYSINAMONTH).map((day) => (
+						<StyledDay
+							active={areDatesTheSame(
+								new Date(),
+								getDateObj(day, currentMonth, currentYear)
+							)}
+						>
 							{day}
-						</span>
+						</StyledDay>
 					))}
-				</div>
+				</CalenderBody>
 			</div>
 		</>
 	);
---
+++ src/pages/Scheduling-management/components/calendar/styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const CalenderBody = styled.div`
+	height: calc(100% - 27px - 40px);
+	display: grid;
+	grid-template-columns: repeat(7, 1fr);
+	grid-template-rows: repeat(${({ fourCol }) => (fourCol ? 4 : 5)}, 1fr);
+`;
+
+export const StyledDay = styled.span`
+    border: 1px solid;
+    text-align: right;
+    padding: 5px;
+    ${({active}) => active && `background: pink`};
+`;
