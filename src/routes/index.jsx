bbdd4f1baf782a5d4fa80bbb5ea9232c7b69247a
Junior042 <eduardoalvesdeoliveirajunior@gmail.com>
2023-08-09 21:45:31 -0300

    merge

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -6,6 +6,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import NotFound from "../pages/NotFound/NotFound";
 import ViewService from "../pages/ViewService";
+import Plans from '../pages/Plans/Plans';
+import Annuncement from '../pages/Annuncement/Annuncement';
+import About from '../pages/About/About';
 
 import { MainManagment } from "../pages/Managment";
 
@@ -28,6 +31,9 @@ const Routers = () => {
         <Route path="*" element={<NotFound />} />
         <Route path="/service/:id" element={<ViewService />} />
         <Route path="/managment" element={<MainManagment />} /> {/* future private route */}
+        <Route path="/annuncement" element={<Annuncement />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/plans" element={<Plans />} />
         {/* <Route path="/post/:id" element={ <Post/>} /> //Passando rotas  */}
       </Routes>
     </BrowserRouter>
