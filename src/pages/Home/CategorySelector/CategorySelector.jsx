782ff316c3cc4f5b8d0f62cf0640927010577bdb
Junior042 <eduardoalvesdeoliveirajunior@gmail.com>
2023-10-11 11:59:03 -0300

    list category api implemented

--- src/pages/Home/CategorySelector/CategorySelector.jsx
+++ src/pages/Home/CategorySelector/CategorySelector.jsx
@@ -1,41 +1,47 @@
 import React, { useState } from "react";
 import "./Style.css";
 import { Carousel } from "primereact/carousel";
+import { useFetch } from "../../../hooks/useFetch";
+import LoadingCategoryCard from "./components/LoadingCategoryCard";
+
+const MOCKIMGCATEGORY =
+  "https://img.freepik.com/fotos-gratis/manicure-mestre-faz-manicure-nas-maos-da-mulher-conceito-de-tratamento-de-spa_186202-7769.jpg";
 
 const MockCategoriesApi = [
-	{
-		name: "Massagem",
-		img_src:
-		  "https://img.freepik.com/fotos-gratis/manicure-mestre-faz-manicure-nas-maos-da-mulher-conceito-de-tratamento-de-spa_186202-7769.jpg",
-	  },
-	  {
-		name: "Massagem 2",
-		img_src:
-		  "https://img.freepik.com/fotos-gratis/manicure-mestre-faz-manicure-nas-maos-da-mulher-conceito-de-tratamento-de-spa_186202-7769.jpg",
-	  },
-	  {
-		name: "Massagem 3",
-		img_src:
-		  "https://img.freepik.com/fotos-gratis/manicure-mestre-faz-manicure-nas-maos-da-mulher-conceito-de-tratamento-de-spa_186202-7769.jpg",
-	  },
-	  {
-		name: "Massagem 4",
-		img_src:
-		  "https://img.freepik.com/fotos-gratis/manicure-mestre-faz-manicure-nas-maos-da-mulher-conceito-de-tratamento-de-spa_186202-7769.jpg",
-	  },
-	  {
-		name: "Massagem 5",
-		img_src:
-		  "https://img.freepik.com/fotos-gratis/manicure-mestre-faz-manicure-nas-maos-da-mulher-conceito-de-tratamento-de-spa_186202-7769.jpg",
-	  },
-	  {
-		name: "Massagem 6",
-		img_src:
-		  "https://img.freepik.com/fotos-gratis/manicure-mestre-faz-manicure-nas-maos-da-mulher-conceito-de-tratamento-de-spa_186202-7769.jpg",
-	  },
+  {
+    name: "Massagem",
+    img_src:
+      "https://img.freepik.com/fotos-gratis/manicure-mestre-faz-manicure-nas-maos-da-mulher-conceito-de-tratamento-de-spa_186202-7769.jpg",
+  },
+  {
+    name: "Massagem 2",
+    img_src:
+      "https://img.freepik.com/fotos-gratis/manicure-mestre-faz-manicure-nas-maos-da-mulher-conceito-de-tratamento-de-spa_186202-7769.jpg",
+  },
+  {
+    name: "Massagem 3",
+    img_src:
+      "https://img.freepik.com/fotos-gratis/manicure-mestre-faz-manicure-nas-maos-da-mulher-conceito-de-tratamento-de-spa_186202-7769.jpg",
+  },
+  {
+    name: "Massagem 4",
+    img_src:
+      "https://img.freepik.com/fotos-gratis/manicure-mestre-faz-manicure-nas-maos-da-mulher-conceito-de-tratamento-de-spa_186202-7769.jpg",
+  },
+  {
+    name: "Massagem 5",
+    img_src:
+      "https://img.freepik.com/fotos-gratis/manicure-mestre-faz-manicure-nas-maos-da-mulher-conceito-de-tratamento-de-spa_186202-7769.jpg",
+  },
+  {
+    name: "Massagem 6",
+    img_src:
+      "https://img.freepik.com/fotos-gratis/manicure-mestre-faz-manicure-nas-maos-da-mulher-conceito-de-tratamento-de-spa_186202-7769.jpg",
+  },
 ];
 
 const CategorySelector = () => {
+  const { data, isFetching, error } = useFetch("/categories");
   const [categories, setCategories] = useState(MockCategoriesApi);
 
   const productTemplate = (category) => {
@@ -54,37 +60,42 @@ const CategorySelector = () => {
     {
       breakpoint: "1024px",
       numVisible: 4,
-	  numScroll: 4,
+      numScroll: 4,
     },
     {
       breakpoint: "991px",
       numVisible: 3,
-	  numScroll: 3,
+      numScroll: 3,
     },
     {
       breakpoint: "767px",
       numVisible: 2,
-	  numScroll: 2,
+      numScroll: 2,
     },
-	{
-		breakpoint: "460px",
-		numVisible: 1,
-		numScroll: 1,
-	  },
+    {
+      breakpoint: "460px",
+      numVisible: 1,
+      numScroll: 1,
+    },
   ];
+
+  console.log(data);
 
   return (
     <section
       id="section-category"
       className="overflow-hidden mt-8 mx-auto px-4 lg:px-10"
     >
-	 <h2 className="h2-title-section">Categorias</h2>
-      <Carousel
-        value={categories}
-        numVisible={5}
-        itemTemplate={productTemplate}
-        responsiveOptions={responsiveOptions}
-      />
+      <h2 className="h2-title-section">Categorias</h2>
+      {isFetching && <LoadingCategoryCard />}
+      {!isFetching && (
+        <Carousel
+          value={data}
+          numVisible={5}
+          itemTemplate={productTemplate}
+          responsiveOptions={responsiveOptions}
+        />
+      )}
     </section>
   );
 };
