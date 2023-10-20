566da211f5308ce5430c5a3370a1edc797c38512
Junior042 <eduardoalvesdeoliveirajunior@gmail.com>
2023-10-20 09:01:04 -0300

    Error handling Service highlighting

--- src/pages/Home/HomeListServices/index.jsx
+++ src/pages/Home/HomeListServices/index.jsx
@@ -4,17 +4,32 @@ import { CardService } from "../../../components";
 import { useFetch } from "../../../hooks/useFetch";
 
 const HomeListServices = () => {
-    const { data, error, isFetching } = useFetch("/services");
+	const { data, error, isFetching } = useFetch("/services");
 
-    return (
-    <>
-      <div className="cards-all-services">
-        {
-            data?.map((service, index) => <CardService key={index} id={service.id} serviceName={service?.name} businessName={service?.business?.name} price={service?.price} rating={service?.averageRating}/>)
-        }
-      </div>
-    </>
-  );
+	return (
+		<>
+			{isFetching && (
+				<h4 className="text-center text-lg">Buscando serviços...</h4>
+			)}
+			{error && (
+				<h4 className="text-center text-lg">
+					Ocorreu um erro inesperado! Tente novamente mais tarde.
+				</h4>
+			)}
+			<div className="cards-all-services">
+				{data?.map((service, index) => (
+					<CardService
+						key={index}
+						id={service.id}
+						serviceName={service?.name}
+						businessName={service?.business?.name}
+						price={service?.price}
+						rating={service?.averageRating}
+					/>
+				))}
+			</div>
+		</>
+	);
 };
 
 export default HomeListServices;
