67e1859cb81f8bab6ffca55cd4765db9ea237ae8
Junior042 <eduardoalvesdeoliveirajunior@gmail.com>
2023-10-31 13:55:02 -0300

    implemented error handling in saved page

--- src/pages/Saved/Saved.jsx
+++ src/pages/Saved/Saved.jsx
@@ -1,35 +1,61 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { AppBar, BtnFillGreen } from "../../components";
 import { usePost } from "../../hooks/useAxios";
 import CardSavedService from "./components/SavedCardService";
 import { api } from "../../services/api";
+import { Toast } from "primereact/toast";
 
 const Saved = () => {
 	const [servicesSaved2, setServicesSaved2] = useState([]);
 	const [checkedList, setCheckedList] = useState([]);
+	const [isFetching, setIsFetching] = useState(true);
+	const [error, setError] = useState();
 	const [servicesSavedStorageAPI, setServicesSavedStorageAPI] = useState(
 		JSON.parse(localStorage.getItem("savedServices"))
 	);
+	const toast = useRef(null);
 
 	const fetchData = async () => {
-		try{
+		try {
 			const response = await api.post("http://localhost:3000/services/many", {
 				servicesId: servicesSavedStorageAPI,
 			});
-			
+
 			setServicesSaved2(response.data);
-			console.log('data api simple fetch', response);
-		} catch(err){
-			console.log('Morreuuuuuuu! ', err)
+			setIsFetching(false);
+			console.log("data api simple fetch", response);
+		} catch (err) {
+			console.log("Morreuuuuuuu! ", err);
+			setError(err);
 		}
-	}
+	};
 
 	useEffect(() => {
-		fetchData();
+		servicesSavedStorageAPI.length > 0 && fetchData();
 	}, []);
 
+	console.log("checklist", checkedList);
+	console.log("error", error);
+	console.log("services storage", servicesSavedStorageAPI);
+	console.log("-------------------------------------");
 
-	console.log(checkedList);
+	const showError = ({ msg }) => {
+		toast.current?.show({
+			severity: "error",
+			summary: "Erro",
+			detail: msg,
+			life: 3000,
+		});
+	};
+
+	const showInfo = ({ msg }) => {
+		toast.current.show({
+			severity: "info",
+			summary: "Lista Atualizada",
+			detail: msg,
+			life: 3000,
+		});
+	};
 
 	const onRemoveServicesToSaved = () => {
 		const servicesStorage = JSON.parse(localStorage.getItem("savedServices"));
@@ -45,11 +71,11 @@ const Saved = () => {
 			setServicesSaved2(
 				servicesSaved2.filter((service) => !checkedList.includes(service?.id))
 			);
+
 			setCheckedList([]);
+
+			showInfo({ msg: "Sua lista de favoritos foi atualizada!" });
 		}
-		
-		setServicesSavedStorageAPI(JSON.parse(localStorage.getItem("savedServices")));
-		setCheckedList([]);
 	};
 
 	return (
@@ -59,11 +85,22 @@ const Saved = () => {
 				<a href="/services">Serviços</a>
 				<a href="/annuncement">Anuncios e eventos</a>
 			</AppBar>
-			{/* {isFetching && <p>Carregando...</p>} */}
 			<main className="max-w-[1440px] m-auto">
-				<BtnFillGreen onclick={() => onRemoveServicesToSaved()}>
-					Remover favoritos
-				</BtnFillGreen>
+				<Toast ref={toast} />
+
+				{checkedList.length > 0 && (
+					<BtnFillGreen onclick={() => onRemoveServicesToSaved()}>
+						Remover favoritos
+					</BtnFillGreen>
+				)}
+
+				<div className="text-center my-8">
+					{errorHandling(error, servicesSaved2, showError, isFetching)}
+				</div>
+
+				{isFetching && !error && servicesSavedStorageAPI.length > 0 && (
+					<p className="text-center my-8 font-bold">Carregando...</p>
+				)}
 				<section
 					className={`flex flex-wrap w-full ${
 						servicesSaved2?.length > 4 && "justify-between"
@@ -85,9 +122,43 @@ const Saved = () => {
 						))}
 				</section>
 			</main>
-			{/* <Shortly /> */}''
 		</>
 	);
 };
 
 export default Saved;
+
+const errorHandling = (
+	error = null,
+	servicesSavedLS = [],
+	showError,
+	isFetching = false
+) => {
+	if (!servicesSavedLS.length > 0) {
+		return (
+			<>
+				<h2 className="font-bold text-2xl">
+					Sua lista de favoritos está vazia!
+				</h2>
+				<p>Dê vida a ela favoritando serviços</p>
+			</>
+		);
+	}
+
+	if (error?.response?.status == 404) {
+		showError({ msg: "Não há serviços salvos!" });
+		return <h2>Services not found!</h2>;
+	}
+
+	if (error) {
+		showError({
+			msg: "Ocorreu algo inesperado. Contate nosso suporte caso continue.",
+		});
+		return (
+			<h2>
+				Ocorreu um erro inesperado, tente novamente mais tarde ou contate nosso
+				suporte.
+			</h2>
+		);
+	}
+};
